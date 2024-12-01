cce80df4d43ba355601e7ece553fc26a1b5d54a5
caaszj <773598627@qq.com>
2024-12-01 10:31:16 +0800

    imp

--- src/opengis/mosaic.py
+++ src/opengis/mosaic.py
@@ -2,38 +2,27 @@ import os
 from osgeo import gdal
 
 def mosaic(path_image, output_dir=None):
-    """
-    :param path_image: 需要镶嵌影像的路径
-    :param output_dir: 输出路径,默认为None时输出到源文件夹
-    :return: None
-    """
     path = path_image
-    # 只获取.tif文件
     path_lists = [f for f in os.listdir(path) if f.lower().endswith('.tif')]
     
     if len(path_lists) < 2:
-        print(f"文件夹 {path} 中没有足够的tif影像进行镶嵌。")
+        print(f"Not enough tif images in folder {path} for mosaic.")
         return
         
-    print(f"正在处理 {len(path_lists)} 个tif影像......")
+    print(f"Processing {len(path_lists)} tif images......")
 
-    # 首先读取所有影像
     images = [gdal.Open(os.path.join(path, img), gdal.GA_ReadOnly) for img in path_lists]
 
-    # 获取第一个影像的投影信息
     input_proj = images[0].GetProjection()
 
     first_image_name = os.path.basename(path_lists[0])
     date_part = first_image_name.split('_')[0]
 
-    # 设置gdal.Warp的选项
     options = gdal.WarpOptions(srcSRS=input_proj, dstSRS=input_proj, format='GTiff',
                              resampleAlg=gdal.GRA_NearestNeighbour)
 
-    # 使用gdal.Warp一次性镶嵌所有影像
     output_filename = f"{date_part}_Mosaic.tif"
     
-    # 如果指定了输出路径,则使用指定路径;否则使用源文件夹
     if output_dir:
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
@@ -43,13 +32,12 @@ def mosaic(path_image, output_dir=None):
         
     gdal.Warp(output_path, images, options=options)
 
-    # 释放影像资源
     for img in images:
         img = None
         del img
 
-    print(f"镶嵌完成,共处理{len(path_lists)}个tif文件")
-    print(f"输出文件为：{output_path}")
+    print(f"Mosaic completed, processed {len(path_lists)} tif files")
+    print(f"Output file: {output_path}")
 
 def get_subfolder_paths(folder_path):
     subfolder_paths = []
